502272a781ec6f91a71b9a80ea4592b97cd5de73
Paramest <paramest.wsw@gmail.com>
2017-07-20 17:31:24 +0700

    update

---
+++ basic/src/app.js
@@ -0,0 +1,12 @@
+import React, { Componrnt } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component{
+    render(){
+        return(
+            <div>
+                cool react code
+            </div>
+        )
+    }
+}
