f2bff429bb2957e11d0a6c901feef6911cdc2fc4
paramest.wsw <paramest.wsw@gmail.com>
2017-08-28 22:25:31 +0700

    update app.js

--- course-purchase/src/App.js
+++ course-purchase/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Coursesales from "./Coursesales";
 
 class App extends Component {
   render() {
+    var course = [
+      { name: 'Complete iOS10 dev course', price: 199},
+      { name: 'Complete pentesting course', price: 200},
+      { name: 'Complete front end dev course', price: 150},
+      { name: 'Bug Bounty and web app pentesting', price: 170}
+    ];
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Course Purchase</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Coursesales item={course} />
       </div>
     );
   }
---
+++ course-purchase/src/Coursesales.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Coursesales extends Component {
+    render(){
+        return(
+            <div>
+            </div>
+        )
+    };
+}
+
+export default Coursesales;
