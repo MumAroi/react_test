bc4239245c4202f247acd780831b941914ec2527
paramest.wsw <paramest.wsw@gmail.com>
2017-08-30 22:00:16 +0700

    update createBrowserHistory

--- route-app/src/index.js
+++ route-app/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route } from 'react-router';
+// createBrowserHistory for react-rout v4^
+import { createBrowserHistory } from 'history'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router history={createBrowserHistory}>
+
+    </Router>
+, document.getElementById('root'));
 registerServiceWorker();
