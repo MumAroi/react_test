07cd4432ea580306fb04731f4b156e3c1231e35d
paramest.wsw <paramest.wsw@gmail.com>
2017-08-30 21:32:10 +0700

    update react route and create route-app

--- mynewrout/src/index.js
+++ mynewrout/src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//  import react rout lib 
+import { BrowserRouter as Router,  Route, Link } from 'react-router-dom';
+// css import
 import './index.css';
+// js import
 import App from './App';
+import Home from './Home';
+import Contact from './Contact'
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <div>
+            <ul>
+                {/* create link */}
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/contact'>Contact-me</Link></li>
+            </ul>
+            <hr />
+
+            <Route exact path="/" component={Home}/>
+            <Route path="/contact" component={Contact}/>
+
+        </div>
+    </Router>    
+, document.getElementById('root'));
 registerServiceWorker();
