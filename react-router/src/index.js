eedf2e3caa41ab7e32933cd09911dd551232700d
Paramest <paramest.wsw@gmail.com>
2017-09-25 16:41:32 +0700

    test NavLink

--- react-router/src/index.js
+++ react-router/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Link, Route, Switch}  from 'react-router-dom';
+import {BrowserRouter, Link, Route, Switch, NavLink}  from 'react-router-dom';
 
 // import './index.css';
 
@@ -14,8 +14,10 @@ ReactDOM.render(
     <BrowserRouter>
         <div>
             <header>
-                <Link to="/" >Home</Link><br/>
-                <Link to="/post" >Post</Link><br/>
+                {/* <Link to="/"   >Home</Link><br/>
+                <Link to="/post"  >Post</Link><br/> */}
+                <NavLink exact to="/"  activeStyle={{color:'red'}} >Home</NavLink><br/>
+                <NavLink to="/post" activeStyle={{color:'red'}} >Post</NavLink><br/>
             </header>
             <Switch>
                 <Route path="/post/:id" component={PostItem}  />
@@ -25,4 +27,5 @@ ReactDOM.render(
         </div>
     </BrowserRouter>
     , document.querySelector('#root'));
+    
 registerServiceWorker();
