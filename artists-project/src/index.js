9f36e62228abe3ff129fb88c37c75d96b401c7d8
Paramest <paramest.wsw@gmail.com>
2017-09-26 16:32:37 +0700

    update artists banner

--- artists-project/src/index.js
+++ artists-project/src/index.js
@@ -1,8 +1,31 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './view/app';
+// import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+// import react lib
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// import page
+import Home from './view/pages/home';
+import Artists from './view/pages/artists';
+// import App from './view/app';
+
+const App = () => {
+    return(
+        <BrowserRouter>
+            <div>
+                <Route exact path="/" component={Home} ></Route>
+                <Route path="/artists/:artistid" component={Artists} ></Route>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
---
+++ artists-project/src/view/pages/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+// import page component
+import Banner from '../component/banner';
+
+class Home extends  Component {
+    render(){
+        return(
+            <div>
+                <Banner/>
+            </div>
+        );
+    }
+}
+
+export default Home;
