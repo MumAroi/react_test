f0efdda14abdfc3600d49b606ea85bed72468345
Paramest <paramest.wsw@gmail.com>
2017-11-27 17:00:47 +0700

    update slides  setting

--- nba/src/components/featured.js
+++ nba/src/components/featured.js
@@ -1,10 +1,45 @@
 import React from 'react';
+// step 25 : import react-slick
+import Slider from 'react-slick';
+
+// step 29 :create slides setting
+const setting = {
+    arrows: false,
+    dots: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScoll: 1
+};
+
+// step 27 : create function generateSlides
+const generateSlides = ({slides}) => {
+    if(slides){
+        return(
+            <Slider {...setting}>
+                {slides.map((item)=>{
+                    return(
+                        <div calssName="item-slider" key={item.id}
+                            style={{background:`url(../images/covers/${item.cover})`}}
+                        >
+                            <div className="caption">
+                                <h4>{item.topic}</h4>
+                                <p>{item.title}</p>
+                            </div>
+                        </div>
+                    );
+                })}
+            </Slider>
+        );
+    }
+}
 
 // step 20 : create featured component
 const Featured = ( props ) => {
-    console.log(props);
     return(
-        <div>slider</div>
+        <div>
+            {/* step 28 : load fuc generateSlides */}
+            { generateSlides(props) }
+        </div>
     );
 }
 
