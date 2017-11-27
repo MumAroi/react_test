dafbc3362e0096baebb2825a7a6a6dc934804e48
Paramest <paramest.wsw@gmail.com>
2017-11-27 14:54:45 +0700

    call api  slider

---
+++ nba/src/components/featured.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// step 20 : create featured component
+const Featured = ( props ) => {
+    console.log(props);
+    return(
+        <div>slider</div>
+    );
+}
+
+export default Featured;
--- nba/src/components/home.js
+++ nba/src/components/home.js
@@ -1,11 +1,38 @@
 // step 10 :  import react and React-Component
 import React, {Component} from 'react';
 
+// step 19 : import component featured
+import Featured from './featured';
+
+// step 24 : create variable URL_HOME
+const URL_HOME = 'http://localhost:3004/home';
+
 // step 11 : create Home class
 class Home extends Component {
+    constructor(props){
+        super(props);
+        // step 22 : create state default
+        this.state = {
+            home: ''
+        }
+    }
+    // step 23 : load data
+    componentDidMount(){
+        fetch(URL_HOME, {method: 'GET'})
+        .then(response =>  response.json())
+        .then(json => {
+            this.setState({ 
+                home: json
+            });
+        });
+    }
+
     render(){
         return(
-            <div>Home</div>
+            <div>
+                {/* step 21 : load featured and put props*/}
+                <Featured slides={this.state.home.slider} />
+            </div>
         );
     }
 }
