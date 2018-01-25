9b925273b03b465d61b35f9a8733a8c48e38b3dc
paramest.wsw <paramest.wsw@gmail.com>
2018-01-25 20:37:45 +0700

    update function search

--- react_redux_v2/src/contrainers/search.js
+++ react_redux_v2/src/contrainers/search.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            keyword: ''
+        }
+    }
+
+    // step 9 : create func handleChange
+    handleChange = (event) => {
+        this.setState({
+            keyword: event.target.value
+        });
+    }
+
+    // strp 10 : use componentDisMount
+    componentDidMount(){
+        console.log(this.state);
+    }
+
+    // step 11 : create func searchCars
+    searchCars = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
     render(){
         return(
             // step 5 : component search
             <div className="main-search">
-                <form>
-                    <input type="text"   />
+                <form onSubmit={this.searchCars}>
+                    <input type="text"  valur={this.state.keyword} onChange={this.handleChange} />
                 </form>
             </div>
         );
