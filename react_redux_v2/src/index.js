3fc177d34b20b169de9a5294e31a96beae450531
paramest.wsw <paramest.wsw@gmail.com>
2018-01-25 20:51:39 +0700

    set up reducers

--- react_redux_v2/src/index.js
+++ react_redux_v2/src/index.js
@@ -3,21 +3,32 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 import registerServiceWorker from './registerServiceWorker';
 
-// step 1 : import BrowserRouter and Route from react router
+// step 1 : BrowserRouter and Route from react router
 import { BrowserRouter, Route } from 'react-router-dom';
-// step 4 : import Care component
+// step 4 : Care component
 import Car from './components/car';
-// step 7 : import css 
+// step 7 : css 
 import './index.css';
+// step 12 : provider 
+import { Provider } from 'react-redux';
+// step 13 : createstore and applymiddleware
+import { createStore, applyMiddleware} from 'redux';
+// step 16 : reducers
+import reducers from './reducers';
+// step 14 : create store
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-    // step 2 : make route
-    <BrowserRouter>
-        <div>
-            {/* step 3 : create route */}
-            <Route exact path="/" component={App} ></Route>
-            <Route path="/car/:id" component={Car}></Route>
-        </div>
-    </BrowserRouter>
+    // step 15 : make provider
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        {/* step 2 : make route */}
+        <BrowserRouter>
+            <div>
+                {/* step 3 : create route */}
+                <Route exact path="/" component={App} ></Route>
+                <Route path="/car/:id" component={Car}></Route>
+            </div>
+        </BrowserRouter>
+    </Provider>
 , document.getElementById('root'));
 registerServiceWorker();
---
+++ react_redux_v2/src/reducers/index.js
@@ -0,0 +1,9 @@
+// step 17 : combinereducers
+import { combineReducers } from 'redux';
+
+// step 18 : make combinereducers
+const rootReducers = combineReducers({
+
+});
+
+export default rootReducers;
