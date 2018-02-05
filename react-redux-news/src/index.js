a29554bea2a7248104d672c5ad6e9c77cc100c25
paramest.wsw <paramest.wsw@gmail.com>
2018-02-05 16:44:06 +0700

    setup reducers

--- react-redux-news/src/index.js
+++ react-redux-news/src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+// step 1 : react redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+// step 4 : reducers
+import reducers from './reducers';
+// step 3 : create store
+const createStoreWidthMiddleware = applyMiddleware(promiseMiddleware)(createStore);
+ReactDOM.render(
+    //  step 2 : set up Provider and reducers
+    <Provider store={createStoreWidthMiddleware(reducers)}>
+        <App />        
+    </Provider>
+, document.getElementById('root'));
 registerServiceWorker();
