eb34df1ea52b88b999306360e462a6a513d3683e
paramest.wsw <paramest.wsw@gmail.com>
2018-02-05 17:57:45 +0700

    add action

--- react-redux-news/src/containers/home.js
+++ react-redux-news/src/containers/home.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 
+// step 9 : redux connect
+import { connect } from 'react-redux';
+// step 10 : load LatestNews
+import LatestNews from '../components/home/latest';
+// step 11 : load action
+import { latestNews } from '../action';
+// step 12 : load bineAction
+import { bindActionCreators } from 'redux'; 
+
 class Home extends Component {
     render(){
         return(
             <div>
-                home
+                <LatestNews/>
             </div>
         );
     }
 }
 
-export default Home;
+// step 13 : map state to props
+function mapStateToProps(state){
+    return{
+        articles: state.articles
+    }
+}
+// step 14 : dispatch reducer
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({latestNews}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
