974e2bb83b0daeb2e1f29adca5035d8fac7ccbf4
paramest <paramest.por@nilecon.com>
2018-07-22 16:57:46 +0700

    add spark line

--- temperature/src/containers/weather_list/weather_list.js
+++ temperature/src/containers/weather_list/weather_list.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Sparklines, SparklinesLine} from 'react-sparklines'
 
 
 class WeatherList extends Component {
 
     renderWeather = (cityData) => {
-        console.log(cityData)
+        // console.log(cityData)
         if(cityData){
             const {id, name} = cityData.city
+            const temps = cityData.list.map( weather => weather.main.temp)
+            
             return(
                 <tr key={id}>
                     <td>{name}</td>
+                    <td>
+                        <Sparklines height={120} width={180} data={temps}>
+                            <SparklinesLine color="red"/>
+                        </Sparklines>
+                    </td>
                 </tr>
             )
         }
